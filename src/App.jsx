7fe580ab9091c9b1dab9a99e179898a5b15d9dd5
salmansaleem2 <salmansaleem8523@gmail.com>
2023-10-01 19:37:50 +0500

    Added favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
+import Hero from "./sections/Hero";
+import CustomerReviews from "./sections/CustomerReviews";
+import Footer from "./sections/Footer";
+import PopularProducts from "./sections/PopularProducts";
+import Services from "./sections/Services";
+import SpecialOffers from "./sections/SpecialOffers";
+import Subscribe from "./sections/Subscribe";
+import SuperQuality from "./sections/SuperQuality";
+import Nav from "./components/Nav";
+
 const App = () => (
   <main className="relative">
-    Navbar
-    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-    <section className="padding">PopularProducts</section>
-    <section className="padding">SuperQuality</section>
-    <section className="padding padding-x py-10">Services</section>
-    <section className="padding">SpecialOffers</section>
-    <section className="bg-pale-blue padding">CustomerReviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffers />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
   </main>
 );
 
