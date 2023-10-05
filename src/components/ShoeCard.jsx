615c67fe8568deacabdb15aed6c8bc7a62e7c30e
salmansaleem2 <salmansaleem8523@gmail.com>
2023-10-05 22:25:34 +0500

    feat: added hero section

---
+++ src/components/ShoeCard.jsx
@@ -0,0 +1,21 @@
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
+  const handleClick = () => {
+    if (bigShoeImg !== imgURL.bigShoe) {
+      changeBigShoeImage(imgURL.bigShoe);
+    }
+  };
+
+  return (
+    <div
+      className={`border-2 rounded-xl
+  ${bigShoeImg === imgURL ? "border-coral-red" : "border-transparent"}
+  cursor-pointer max-sm:flex-1
+  `}
+      onClick={handleClick}
+    >
+      ShoeCard
+    </div>
+  );
+};
+
+export default ShoeCard;
